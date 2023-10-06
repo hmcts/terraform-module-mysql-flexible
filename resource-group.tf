e9b263af77164bf52fa91e36146997180ea5ddcf
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-06 12:01:28 +0100

    fix: rg name

--- resource-group.tf
+++ resource-group.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "new" {
   count = var.existing_resource_group_name == null ? 1 : 0
 
-  name     = "${local.name}-type-${var.env}"
+  name     = "${local.name}-rg-${var.env}"
   location = var.location
 
   tags = var.common_tags
